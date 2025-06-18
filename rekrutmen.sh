f9245a85bc6800f8f2c52d43987bc35521810343
dinirst <ristanti@medify.id>
2025-06-18 14:44:57 +0700

    small changes to rekrutmen.sh

--- rekrutmen.sh
+++ rekrutmen.sh
@@ -8,23 +8,23 @@ remote_branch="${remote_branch:-development}"
 cd /var/www/rekrutmen || { echo "Directory not found"; exit 1; }
 echo "Navigated to /var/www/rekrutmen."
 
-git remote remove rekrutmen && echo "Removed 'rekrutmen'."
+php8.2 artisan recreate-db && echo "--DB recreated"
 
-git checkout development && echo "Switched to 'development'."
+git remote remove rekrutmen && echo "--Removed 'rekrutmen'."
 
-git branch -D rekrutmen && echo "Deleted 'rekrutmen'."
+git checkout development && echo "--Switched to 'development'."
+
+git branch -D rekrutmen && echo "--Deleted 'rekrutmen'."
 
 
-git remote add rekrutmen "$remote_url" && echo "Added remote: $remote_url"
+git remote add rekrutmen "$remote_url" && echo "--Added remote: $remote_url"
 
-git fetch rekrutmen && echo "Fetched 'rekrutmen'."
+git fetch rekrutmen && echo "--Fetched 'rekrutmen'."
 
-git checkout -B rekrutmen rekrutmen/"$remote_branch" && echo "Switched 'rekrutmen/$remote_branch'."
+git checkout -B rekrutmen rekrutmen/"$remote_branch" && echo "--Switched 'rekrutmen/$remote_branch'."
 
 echo "\nGit operations completed successfully."
 echo "=========="
-
-php8.2 artisan recreate-db
 
 php8.2 artisan migrate
 
